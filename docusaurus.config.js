a5d4cc0b353b0175f20391521b60f884775b1a23
Sourav Santra <55660986+Sourav59580@users.noreply.github.com>
2024-10-26 11:17:17 +0000

    feature: document update with new details

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,17 +74,17 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: '',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'valuit Logo',
+          src: 'img/logo_light.svg',
         },
         items: [
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
